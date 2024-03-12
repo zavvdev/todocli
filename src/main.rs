5959c2bc4cfe350a7d7bc841873bdedd005a25d5
zavvdev <zaviryukha.igor99@gmail.com>
2024-03-12 21:54:13 +0200

    added task and list

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,91 @@
 use std::io::{self, Write};
 
-// exit            - terminate the program
-// help            - display help message
-// list            - list all items or options
-// add [list name] - add a new item or option
-// remove [...ids] - remove items or options
-// done [...ids]   - mark items or options as done
-// undone [...ids] - mark items as undone
-// clear           - remove all items or options
-// save            - save the current list to a file
-// load            - load a list from a file
+struct Task {
+    id: u64,
+    text: String,
+    is_done: bool,
+}
+
+impl Task {
+    fn new(id: u64, text: String) -> Self {
+        Self {
+            id,
+            text,
+            is_done: false,
+        }
+    }
+
+    fn done(&mut self) {
+        self.is_done = true;
+    }
+
+    fn undone(&mut self) {
+        self.is_done = false;
+    }
+}
+
+struct List {
+    tasks: Vec<Task>,
+}
+
+impl List {
+    fn new() -> Self {
+        Self { tasks: Vec::new() }
+    }
+
+    fn add(&mut self, task: String) {
+        match self.tasks.last() {
+            Some(last) => {
+                let next_id = last.id + 1; // TODO: Check for integer overflow
+                self.tasks.push(Task::new(next_id, task));
+            }
+            None => {
+                self.tasks.push(Task::new(1, task));
+            }
+        }
+    }
+
+    fn remove(&mut self, id: u64) {
+        self.tasks.retain(|t| t.id != id);
+    }
+
+    fn clear(&mut self) {
+        self.tasks.clear();
+    }
+
+    fn mark_task_done(&mut self, id: u64) {
+        for task in &mut self.tasks {
+            if task.id == id && !task.is_done {
+                task.done();
+                break;
+            }
+        }
+    }
+
+    fn mark_task_undone(&mut self, id: u64) {
+        for task in &mut self.tasks {
+            if task.id == id && task.is_done {
+                task.undone();
+                break;
+            }
+        }
+    }
+
+    fn view(&mut self) {
+        for task in &mut self.tasks {
+            println!("{} | {}", task.id, task.text);
+        }
+    }
+}
+
+enum ProcessResult {
+    Exit,
+    UnknownCommand,
+    Ok,
+}
 
 const EXIT: &str = "exit";
+const HELP: &str = "help";
 
 fn trim_whitespaces(s: &str) -> String {
     let words: Vec<_> = s.split_whitespace().collect();
@@ -22,13 +96,42 @@ fn exit() {
     println!("Bye!");
 }
 
-fn process_input(input: &str) -> bool {
+fn help() {
+    println!("Available commands:");
+    println!("exit                - terminate the program");
+    println!("help                - display help message");
+    println!("list                - print all tasks");
+    println!("add [task]          - add new task");
+    println!("remove [...task_id] - remove items or options");
+    println!("done [...task_id]   - mark items or options as done");
+    println!("undone [...task_id] - mark items as undone");
+    println!("clear               - remove all tasks");
+    println!("save                - save the current list to file");
+    println!("load                - load a list from a file");
+}
+
+fn process_input(input: &str, list: &mut List) -> ProcessResult {
     if input == EXIT {
         exit();
-        return true;
+        return ProcessResult::Exit;
     }
 
-    false
+    if input == HELP {
+        help();
+        return ProcessResult::Ok;
+    }
+
+    if input == "add" {
+        list.add("hello".to_string());
+        return ProcessResult::Ok;
+    }
+
+    if input == "list" {
+        list.view();
+        return ProcessResult::Ok;
+    }
+
+    ProcessResult::UnknownCommand
 }
 
 fn prepare_input(s: &str) -> String {
@@ -47,13 +150,17 @@ fn accept_input() -> String {
 }
 
 fn main() {
+    let mut list = List::new();
+
     loop {
         print!("> ");
 
-        if process_input(&prepare_input(&accept_input())) {
-            break;
+        match process_input(&prepare_input(&accept_input()), &mut list) {
+            ProcessResult::Exit => break,
+            ProcessResult::UnknownCommand => {
+                println!("Unknown command");
+            }
+            ProcessResult::Ok => {}
         }
-
-        println!("Unknown command");
     }
 }
